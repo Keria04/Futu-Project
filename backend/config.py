acf4b1cd714005ea7f8c20153cdd23fbbde95017
qadg <qadgmain@gmail.com>
2025-05-19 21:05:45 +0800

    测试功能

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,16 @@ input_size = 224  # 输入图像的大小
 batchsize = 128
 normalize_mean = [0.485, 0.456, 0.406]  # 图像预处理的均值
 normalize_std = [0.229, 0.224, 0.225]   # 图像预处理的标准差
+
+# config/faiss_config.py
+import os
+# 特征文件路径
+FEATURE_PATH = os.path.join("backend","features.npy")
+# 图像 ID 文件路径（可选）
+ID_PATH = os.path.join("backend", "ids.npy")
+# FAISS 索引文件保存路径
+INDEX_PATH = os.path.join("backend", "faiss_module", "image.index")
+# 特征维度（如 ResNet 输出为 512）
+VECTOR_DIM = 2048
+# IVF 索引中的聚类数量（影响召回速度与精度）
+N_LIST = 7
