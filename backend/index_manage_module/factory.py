dce402e582096eed654fa0d6d1dc38107f46baec
qadg <qadgmain@gmail.com>
2025-05-30 16:39:35 +0800

    新增向量查询功能

--- backend/index_manage_module/factory.py
+++ backend/index_manage_module/factory.py
@@ -10,3 +10,12 @@ def build_index(dataset_dir, dataset_name, distributed=False):
     """
     builder = IndexBuilder(dataset_dir, dataset_name, distributed=distributed)
     return builder.build()
+
+def get_all_image_features(dataset_name):
+    """
+    获取指定数据集下所有图片的id和特征向量
+    :param dataset_name: 数据集名称
+    :return: List[Tuple[int, np.ndarray]]
+    """
+    builder = IndexBuilder(None, dataset_name)
+    return builder.get_all_image_features(dataset_name)
