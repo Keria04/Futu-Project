78b8c313723805abe61a611409174ffa8fc13215
qadg <qadgmain@gmail.com>
2025-06-25 09:34:46 +0800

    整理旧文件

--- backend/control_service/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API路由初始化
-"""
-from .search_api import search_bp
-from .index_api import index_bp
-from .dataset_api import dataset_bp
-from .image_api import image_bp
-
-__all__ = ['search_bp', 'index_bp', 'dataset_bp', 'image_bp']
